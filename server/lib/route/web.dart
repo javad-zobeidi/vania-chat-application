da75ef59d31df080894c20be6a3ebf4b83763718
Javad Zobeidi <j.zobeidi89@gmail.com>
2024-06-23 09:50:09 +0330

    Update

--- server/lib/route/web.dart
+++ server/lib/route/web.dart
@@ -1,32 +1,7 @@
 import 'package:vania/vania.dart';
-import '../ddrt.dart';
 
 class WebRoute implements Route {
   @override
-  void register() {
-    Router.get("/", () {
-      final context = {
-        'title': 'This is just a Vania Test',
-        'isLoggedIn': true,
-        'isGuest': false,
-        'username': 'Javad',
-        'users': [
-          {
-            'name': 'Vania',
-            'email': 'info@vdart.dev',
-            'roles': ['admin', 'user']
-          },
-          {
-            'name': 'Javad',
-            'email': 'javad@vdart.dev',
-            'roles': ['user']
-          },
-        ],
-        'userType': 'admin',
-        'keepLooping': true
-      };
-
-      return view('index', context);
-    });
+  void register() {}
   }
 }
